1c318957086dc6e5d286dee585ae9c56f8ead9c4
Tebs Yap <43737102+tebanski@users.noreply.github.com>
2018-11-17 09:21:36 -0500

    Update packet.py

--- lib/packet.py
+++ lib/packet.py
@@ -16,21 +16,16 @@ else:
 # PACKET_FORMAT = "QLLBBffffffffffff"
 # PACKET_SIZE_B = 18 + 46 + 2
 
-HEADER_SIZE_B = 22
-
-TAIL_SIZE_B = 2
-
-
 PACKET_TYPE_9DOF = 4
 
 PACKET_FORMAT_9DOF = "QLLBBLfffffffff"
 
-PACKET_SIZE_9DOF_B = HEADER_SIZE_B + 36 + TAIL_SIZE_B
+PACKET_SIZE_9DOF_B = 60
 
 FIELD_NAMES_9DOF = (
     "timestamp_us", "org_tag", "unit_tag", "packet_type", "status", "chksum",
     "accel_x", "accel_y", "accel_z", "gyro_x", "gyro_y", "gyro_z",
-    "mag_x", "mag_y", "mag_x")
+    "mag_x", "mag_y", "mag_z")
 
 Packet9DOF = namedtuple("Packet9DOF", FIELD_NAMES_9DOF)
 
@@ -39,7 +34,7 @@ PACKET_TYPE_GPS = 8
 
 PACKET_FORMAT_GPS = "QLLBBLdddd"
 
-PACKET_SIZE_GPS_B = HEADER_SIZE_B + 32 + TAIL_SIZE_B
+PACKET_SIZE_GPS_B = 56
 
 FIELD_NAMES_GPS = (
     "timestamp_us", "org_tag", "unit_tag", "packet_type", "status", "chksum",
@@ -52,12 +47,12 @@ PACKET_TYPE_FUSION = 16
 
 PACKET_FORMAT_FUSION = "QLLBBLfffffffffdddd"
 
-PACKET_SIZE_FUSION_B = HEADER_SIZE_B + 68 + TAIL_SIZE_B
+PACKET_SIZE_FUSION_B = 96
 
 FIELD_NAMES_FUSION = (
     "timestamp_us", "org_tag", "unit_tag", "packet_type", "status", "chksum",
     "accel_x", "accel_y", "accel_z", "gyro_x", "gyro_y", "gyro_z",
-    "mag_x", "mag_y", "mag_x", "lat_deg", "long_deg", "alt_m", "vel")
+    "mag_x", "mag_y", "mag_z", "lat_deg", "long_deg", "alt_m", "vel")
 
 PacketFusion = namedtuple("PacketFusion", FIELD_NAMES_FUSION)
 
@@ -71,7 +66,7 @@ def unpack_log(file_path, packet_format, packet_size,
         _buffer = fh.read(packet_size)
         if _buffer:
           try:
-            sample = srtuct.unpack_from(packet_format, _buffer)
+            sample = struct.unpack_from(packet_format, _buffer)
           except:
             continue
           else:
@@ -107,72 +102,76 @@ def unpack_fusion_log(file_path, as_tuple=False):
   return None
 
 
-def unpack_logs(dir_path, file_type=None, as_tuple=False, consolidate=True):
-  unpacked = deque()
-  if file_type.lower() in ("9dof", "dof",):
-    funcs = (unpack_9dof_log,)
-    fn_names = ("9dof", )
-  elif file_type.lower() in ("gps",):
-    funcs = (unpack_gps_log,)
-    fn_names = ("gps",)
-  elif file_type.lower() in ("fusion", "fuse",):
-    funcs = (unpack_fusion_log,)
-    fn_names = ("fusion",)
-  else:
-    if not consolidate:
-      unpacked = dict(dof=deque(), gps=deque(), fusion=deque())
-    funcs = (unpack_9dof_log, unpack_gps_log, unpack_fusion_log,)
-    fn_names = ("dof", "gps", "fusion")
-  for file_name in os.listdir(dir_path):
-    file_path = os.path.join(dir_path, file_name)
-    for (fn_name, func) in zip(fn_names, funcs):
-      samples = func(file_path, as_tuple=as_tuple)
-      if samples:
-        if consolidate:
-          unpacked.extend(samples)
-        else:
-          unpacked[fn_name].extend(samples)
-        continue
-  return unpacked
-
-
-def unpack_to_csv(dir_path, csv_path):
-  timestamp = int(time.time() * 1000000)
-  unpacked = unpack_logs(
-      dir_path, file_type=None, as_tuple=False, consolidate=False)
-  for (file_type, samples) in unpacked.items():
+def unpack_logs(log_dir, log_names=None, as_tuple=False):
+  unpacked = dict()
+  if not log_names:
+    log_names = os.listdir(log_dir)
+  for log_name in log_names:
+    log_path = os.path.join(log_dir, log_name)
+    if log_name.find("_FUSE_") >= 0:
+      samples = unpack_fusion_log(log_path, as_tuple=as_tuple)
+      log_type = "fusion"
+    elif log_name.find("_9DOF_") >= 0:
+      samples = unpack_9dof_log(log_path, as_tuple=as_tuple)
+      log_type = "dof"
+    elif log_name.find("_GPS_") >= 0:
+      samples = unpack_gps_log(log_path, as_tuple=as_tuple)
+      log_type = "gps"
+    else:
+      continue
     if samples:
-      if file_type == "dof":
-        column_names = Packet9DOF._fields
-      elif file_type == "gps":
-        columns_names = PacketGPS._fields
-      elif file_type == "fusion":
-        column_names = PacketFusion._fields
-      file_name = "{0}_{1}.csv".format(timestamp, file_type)
-      file_path = os.path.join(csv_path, file_name)
-      with open(file_path, "w") as fh:
+      if log_type not in unpacked:
+        unpacked[log_type] = deque()
+      unpacked[log_type].extend(samples)
+    return unpacked
+
+
+def unpack_to_csv(log_dir, log_names=None, csv_dir=None, csv_file=None):
+  timestamp = int(time.time() * 1000000)
+  unpacked = unpack_logs(log_dir, log_names=log_names, as_tuple=False)
+  csv_paths = list()
+  for (log_type, samples) in unpacked.items():
+    if samples:
+      if log_type == "fusion":
+        column_names = FIELD_NAMES_FUSION
+      elif log_type == "dof":
+        column_names = FIELD_NAMES_9DOF
+      elif log_type == "gps":
+        columns_names = FIELD_NAMES_GPS
+      else:
+        continue
+      if not csv_dir:
+        csv_dir = log_dir
+      if not csv_file:
+        csv_file = "{0}_{1}.csv".format(timestamp, log_type.upper())
+      csv_path = os.path.join(csv_dir, csv_file)
+      csv_paths.append(csv_path)
+      with open(csv_path, "w") as fh:
         column_names = ",".join(list(column_names))
         fh.write(column_names + "\n")
         for sample in samples:
           sample = ",".join([str(value) for value in sample])
-          fh.write(sample, + "\n")
+          fh.write(sample + "\n")
+  return tuple(csv_paths)
 
 
-def unpack_to_dframe(dir_path):
+def unpack_to_dframe(log_dir, log_names=None):
   if PANDAS_AVAILABLE:
-    unpacked = unpack_logs(
-        dir_path, file_type=None, as_tuple=False, consolidate=False)
-    for (file_type, samples) in unpacked.items():
+    unpacked = unpack_logs(log_dir, log_names=log_names, as_tuple=False)
+    for (log_type, samples) in unpacked.items():
       if samples:
-        if file_type == "dof":
-          column_names = Packet9DOF._fields
-        elif file_type == "gps":
-          columns_names = PacketGPS._fields
-        elif file_type == "fusion":
-          column_names = PacketFusion._fields
-        _df = pd.DataFrame(samples, columns=column_names)
-        _df.loc[:, "timestamp"] = pd.to_datetime(_df["timestamp_us"])
-        _df = _df.set_index("timestamp").sort_index()
-        unpacked[file_type] = _pd
+        if log_type == "fusion":
+          _df = pd.DataFrame(list(samples), columns=FIELD_NAMES_FUSION)
+        elif log_type == "dof":
+          _df = pd.DataFrame(list(samples), columns=FIELD_NAMES_9DOF)
+        elif log_type == "gps":
+          _df = pd.DataFrame(list(samples), columns=FIELD_NAMES_GPS)
+        else:
+          continue
+        if not _df.empty and "timestamp_us" in _df:
+          _df.loc[:, "datetime"] = pd.to_datetime(_df["timestamp_us"],
+                                                  unit="us")
+          _df = _df.set_index("datetime").sort_index()
+        unpacked[log_type] = _df
     return unpacked
   return None
